b1985e56edaa57f73d5b9d25694b5754bded4ca2
EJFrads <ejf549@york.ac.uk>
2020-10-22 16:57:21 +0100

    Plot of the data

--- tidy_data.R
+++ tidy_data.R
@@ -33,3 +33,37 @@ chaff2 %>% group_by(gender) %>%
 t.test(data = chaff2,
        mass ~ gender,
        var.equal = T)
+
+##t test shows females have a significantly lower mass than males
+
+
+
+##create a figure for chaff2
+library(ggplot2)
+
+fig1 <- ggplot(chaff2, aes(gender, mass, fill = gender)) +
+         geom_boxplot(alpha = 0.5, fill = c("darkgreen", "lightgray")) +
+         geom_jitter(colour = "black",size = 0.6,width = 0.2)
+
+
+# figure saving settings
+units <- "in"  
+fig_w <- 3.5
+fig_h <- fig_w
+dpi <- 300
+device <- "tiff" 
+
+ggsave("figures/fig1.tiff",
+       plot = fig1,
+       device = device,
+       width = fig_w,
+       height = fig_h,
+       units = units,
+       dpi = dpi)
+
+
+
+
+
+
+
